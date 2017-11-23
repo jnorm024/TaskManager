6b75e4706fdfe09f0f05aba6349e5c1610503d09
Jeremy Normandin <jnorm024@uottawa.ca>
2017-11-23 15:40:13 -0500

    Version de base de la liste de récompenses ainsi que la confirmation. Je risque de changer la confirmation par un pop-up.

---
+++ app/src/main/java/com/example/jeremynormandin/taskmanager/giveRewards.java
@@ -0,0 +1,17 @@
+package com.example.jeremynormandin.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class giveRewards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give_rewards);
+        getSupportActionBar().setTitle("Give a reward");
+
+
+
+    }
+}
